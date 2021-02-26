7bf880c5074da33c54ee25bd8a0a77e4344213ee
davidfdriscoll <david.f.driscoll@gmail.com>
2021-02-25 18:13:50 -0800

    Added basic HTML to React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="main-head">
+        <img alt="" src="apotheosis_homer_small.jpeg" />
+        <div id="author-title">
+          <h3 id="author">R.J. Cunliffe</h3>
+          <h2 id="title">A Lexicon of the Homeric Dialect</h2>
+        </div>
+        <form>
+          <input type="text" name="item" id="item" />
+          <button>Search</button>
+        </form>
       </header>
+      <article className='content'>
+        <h3 className="lemma"></h3>
+        <p className="definition"></p>
+      </article>
+      <aside className="sidebar">
+        <ul className="nav">
+          <li className="selected">ἀγείρω</li>
+          <li>ἀγείρω</li>
+          <li>ἀγείρω</li>        
+        </ul>
+      </aside>
     </div>
   );
 }
