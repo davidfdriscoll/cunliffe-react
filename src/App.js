20d6fbb0c83db5200ded8de8933a0c7bd4eda8c2
davidfdriscoll <david.f.driscoll@gmail.com>
2021-03-01 10:34:37 -0800

    Restored container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import DefinitionDisplay from "./components/DefinitionDisplay";
 import SearchBar from "./components/SearchBar";
 
