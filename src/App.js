1511f575e77902f74e8a91740bd04a969a09a7d1
davidfdriscoll <david.f.driscoll@gmail.com>
2021-03-01 10:34:08 -0800

    Removed styles from app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import DefinitionDisplay from "./components/DefinitionDisplay";
 import SearchBar from "./components/SearchBar";
 
-const useStyles = makeStyles((theme) => ({
-
-}));
-
 function App() {
-  const classes = useStyles();
   return (
     <Container maxWidth="md">
         <SearchBar />
