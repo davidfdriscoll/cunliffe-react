68c7c51dca82c64c4d2438ee8f6dce0a832fcdff
davidfdriscoll <david.f.driscoll@gmail.com>
2021-03-03 18:04:05 -0800

    Changed app to class with constructor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import DefinitionDisplay from "./components/DefinitionDisplay";
 import SearchBar from "./components/SearchBar";
 
-function App(props) {
-  const [currentWord, setCurrentWord] = useState(props.words[0]);
+class App extends Component {
 
-  function onHeadword(newWord) {
-    setCurrentWord(newWord);
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: props.words,
+      currentWord: props.words[0]
+    }
+
+    // https://reactjs.org/docs/handling-events.html
+    this.onHeadword = this.onHeadword.bind(this);
   }
-  return (
-    <div>
-      <CssBaseline />
-      <SearchBar words={props.words} onHeadword={onHeadword} /> 
-      <Container disableGutters maxWidth="md">
-        <DefinitionDisplay word={currentWord} />
-      </Container>
-    </div>
-  );
+
+  onHeadword(newWord) {
+    this.setState({ currentWord : newWord });
+  }
+
+  render() {
+    return(
+      <div>
+        <CssBaseline />
+        <SearchBar words={this.state.words} onHeadword={this.onHeadword} /> 
+        <Container disableGutters maxWidth="md">
+          <DefinitionDisplay word={this.state.currentWord} />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
